1f4e536abf403ea1b305914d0ca0988e23c9805e
subhanireddy88 <73657708+subhanireddy88@users.noreply.github.com>
2020-12-06 22:22:53 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
 	    'sonar.login': 'admin',
-	    'sonar.password': '4ee2b81c75c2aa1a3c19e38207776c131ef3d0d1',
+	    'sonar.password': '258cdc8799840718ef7006c0db3006da55b521b7',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
